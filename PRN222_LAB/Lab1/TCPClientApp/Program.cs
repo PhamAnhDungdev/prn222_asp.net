959f8111c285439500d06deae4ff1a6061ad5c16
Phạm Anh Dũng <168076412+PhamAnhDungdev@users.noreply.github.com>
2025-01-10 09:40:21 +0700

    Init lab1

---
+++ PRN222_LAB/Lab1/TCPClientApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TCPClientApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
