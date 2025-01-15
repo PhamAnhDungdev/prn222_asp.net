b6c26ddac6bb2616133d9074a8d9d87710f76585
Phạm Anh Dũng <168076412+PhamAnhDungdev@users.noreply.github.com>
2025-01-15 11:36:48 +0700

    Push slot3

---
+++ Slot3/TaskDemonstration/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace TaskDemonstration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+            Task task01 = new Task(() => PrintNumber("Dung chao ban lan"));
+            task01.Start();
+
+            Task task02 = Task.Run(delegate
+            {
+                PrintNumber("Dung lai chao ban lan");
+            });
+
+            Task task03 = new Task(new Action(() =>
+            {
+                PrintNumber("Dung tiep tuc chao ban lan");
+            }));
+            task03.Start();
+            Console.WriteLine($"Thread '{Thread.CurrentThread.Name}'");
+            Console.ReadKey(); // Chờ người dùng ấn 1 phím bất kì.
+        }
+
+        static void PrintNumber(string message)
+        {
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.WriteLine($"{message}: {i}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
